636e15d3d47cdb64727f7c33560d3525d5762c30
Alfonso Mulsa Fau <amulsa@gmail.com>
2024-04-02 09:45:47 +0200

    Agregar archivos de proyecto.

---
+++ TypescriptPrueba/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TypescriptPrueba.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
