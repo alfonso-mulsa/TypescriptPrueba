bcd830ec3a69dbf0ea0413918da627804c7c9422
Alfonso Mulsa Fau <amulsa@gmail.com>
2024-04-03 09:10:01 +0200

    Revert "cambios"
    This reverts commit c47ae1bced8b0a3b1823fb636e3276dc71618670.

--- ListaDeNumeros/wwwroot/js/file.js
+++ ListaDeNumeros/wwwroot/js/file.js
@@ -1,41 +1,27 @@
 function crearLista() {
-    borrarLista();
     var numero = tomaVal("idNumero");
     for (var contador = 1; contador < numero; contador++) {
-        document.getElementById("idLista").appendChild(crearLi(String(contador)));
+        document.getElementById("idLista").appendChild(crearLinea(String(contador)));
     }
 }
 function tomaVal(nombreOperando) {
     return Number(document.getElementById(nombreOperando).value);
 }
-function crearLi(texto) {
-    var nuevoLi = document.createElement("li");
-    nuevoLi.textContent = texto;
-    return nuevoLi;
-}
-function borrarUltimoLi() {
-    var numLineas = document.getElementById("idLista").childElementCount;
-    if (numLineas > 0) {
-        var ultimoLi = document.getElementById("idLista").lastChild;
-        document.getElementById("idLista").removeChild(ultimoLi);
-    }
+function crearLinea(texto) {
+    var nuevaLinea = document.createElement("li");
+    nuevaLinea.textContent = texto;
+    return nuevaLinea;
 }
 function borrarLista() {
     var numLineas = document.getElementById("idLista").childElementCount;
     for (var contador = 1; contador <= numLineas; contador++) {
-        borrarLi("idLista");
+        borrarLinea("idLista");
     }
 }
-function borrarLi(idElemento) {
-    var linea = document.querySelector("#" + idElemento + ">li");
+function borrarLinea(idElemento) {
+    var linea = document.querySelector("#" + idElemento + " li");
     document.getElementById(idElemento).removeChild(linea);
 }
-function crearEvento(idElemento, nombreEvento, nombreFuncion) {
-    document.getElementById(idElemento).addEventListener(nombreEvento, nombreFuncion);
-}
-crearEvento("idCrearLista", "click", crearLista);
-crearEvento("idBorrarUltimoLi", "click", borrarUltimoLi);
-crearEvento("idBorrarLista", "click", borrarLista);
-//document.getElementById("idCrearLista").addEventListener("click", crearLista);
-//document.getElementById("idBorrarLista").addEventListener("click", borrarLista);
+document.getElementById("idCrearLista").addEventListener("click", crearLista);
+document.getElementById("idBorrarLista").addEventListener("click", borrarLista);
 //# sourceMappingURL=file.js.map
--- ListaDeNumeros/wwwroot/js/file.ts
+++ ListaDeNumeros/wwwroot/js/file.ts
@@ -1,42 +1,27 @@
 ﻿function crearLista() {
-    borrarLista();
     let numero = tomaVal("idNumero");
     for (let contador = 1; contador < numero; contador++) {
-        document.getElementById("idLista").appendChild(crearLi(String(contador)));
+        document.getElementById("idLista").appendChild(crearLinea(String(contador)));
     }
 }
 function tomaVal(nombreOperando: string) {
     return Number((<HTMLInputElement>document.getElementById(nombreOperando)).value);
 }
-function crearLi(texto: string) {
-    let nuevoLi = document.createElement("li");
-    nuevoLi.textContent = texto;
-    return nuevoLi;
-}
-function borrarUltimoLi() {
-    let numLineas = document.getElementById("idLista").childElementCount;
-    if (numLineas > 0) {
-        let ultimoLi = document.getElementById("idLista").lastChild;
-        document.getElementById("idLista").removeChild(ultimoLi);
-    }
+function crearLinea(texto: string) {
+    let nuevaLinea = document.createElement("li");
+    nuevaLinea.textContent = texto;
+    return nuevaLinea;
 }
 function borrarLista() {
     let numLineas = document.getElementById("idLista").childElementCount;
     for (let contador = 1; contador <= numLineas; contador++) {
-        borrarLi("idLista");
+        borrarLinea("idLista");
     }
 }
-function borrarLi(idElemento: string) {
-    let linea = document.querySelector("#" + idElemento + ">li");
+function borrarLinea(idElemento: string) {
+    let linea = document.querySelector("#" + idElemento + " li");
     document.getElementById(idElemento).removeChild(linea);
 }
-function crearEvento(idElemento: string, nombreEvento: string, nombreFuncion) {
-    document.getElementById(idElemento).addEventListener(nombreEvento, nombreFuncion);
-}
 
-crearEvento("idCrearLista", "click", crearLista);
-crearEvento("idBorrarUltimoLi", "click", borrarUltimoLi);
-crearEvento("idBorrarLista", "click", borrarLista);
-
-//document.getElementById("idCrearLista").addEventListener("click", crearLista);
-//document.getElementById("idBorrarLista").addEventListener("click", borrarLista);
+document.getElementById("idCrearLista").addEventListener("click", crearLista);
+document.getElementById("idBorrarLista").addEventListener("click", borrarLista);
